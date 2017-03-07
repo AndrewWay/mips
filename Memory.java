88140c0795e605b5a98fac06b7d40cad26647151
Andrew <arw405@mun.ca>
2017-03-06 21:54:10 -0330

    Reorganized code into methods. Created methods for reading and writing to DataMemory

--- Memory.java
+++ Memory.java
@@ -3,6 +3,7 @@ public class Memory extends Stage{
 	
 	Bin[] dataMemory = new Bin[10000];//Are the bins inside the array initialized at any point??
 	Bin readData=new Bin(1);//Default size of array
+	Bin Mux12_Output,Readdata2,ALUResult,Zero,AddResult,CV;
 	
 	public Memory(Firmware m) {
 		super(m);
@@ -12,25 +13,47 @@ public class Memory extends Stage{
 	}
 	
 	public void memory(){
-		
-		if((getIBuffSeg(1).getArray()[1]) != 0 && (getMem().getControlVector().getArray()[4] != 0)) {
-			//Send input for multiplexer in fetch stage
-			//Need to wait for multiplexer objects
-		}
-		
-		Bin writeData = getIBuffSeg(3); //Data to write
-		Bin readAddress = getIBuffSeg(2), writeAddress = getIBuffSeg(2); //Address for read or write
-		
-		if(getMem().getControlVector().getArray()[5] != 0) { //MemRead
+		CV=getMem().getControlVector();
+		readIngoingBuffer();
+		andGate();
+		Bin writeData = Readdata2; //Data to write
+		Bin readAddress = ALUResult, writeAddress = ALUResult; //Address for read or write
+		if(CV.getArray()[5] != 0) { //MemRead
 			//Read from Data Memory
 			readData = dataMemory[readAddress.evaluate()]; //Update when data memory is constructed.
-		} else if(getMem().getControlVector().getArray()[6] != 0) { //MemWrite
+		} else if(CV.getArray()[6] != 0) { //MemWrite
 			//Write to Data Memory
 			dataMemory[writeAddress.evaluate()] = writeData;
 		}
+		loadOutgoingBuffer();
+	}
+	public void loadOutgoingBuffer(){
 		loadBuffer(2,readData);
 		loadBuffer(1,getIBuffSeg(2));
-		System.out.println("MEMORY BUFF SEG 1: "+getIBuffSeg(2).evaluate());
 		loadBuffer(0,getIBuffSeg(0));
+	}
+	public void andGate(){
+		if((Zero.getArray()[0]) != 0 && (getMem().getControlVector().getArray()[4] != 0)) {
+			//Send input for multiplexer in fetch stage
+			//Need to wait for multiplexer objects
+		}
+	}
+	public void readIngoingBuffer(){
+		Mux12_Output=getIBuffSeg(0);
+		Readdata2=getIBuffSeg(1);
+		ALUResult=getIBuffSeg(2);
+		Zero=getIBuffSeg(3);
+		AddResult=getIBuffSeg(4);
+		CV=getIBuffSeg(5);;
+	}
+	public Bin getReadData(int i){
+		return dataMemory[i];
+	}
+	public void writeData(int i,Bin b){
+		if(b.getBinSize()==32){
+			dataMemory[i]=b;//Return 32 bit Bin
+		}
+		else
+			System.out.println("MEMORY WRITE ERROR: Input data not 32bits");
 	}
 }
